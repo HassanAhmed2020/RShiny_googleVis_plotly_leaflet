c6ff8f6b734b21aefbfacc627b85b81a75923485
HassanAhmed2020 <somii@hotmail.com>
2019-04-11 18:40:49 +0000

    Commit

--- ui.R
+++ ui.R
@@ -13,16 +13,28 @@ library(shiny)
 shinyUI(fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Normal Distribution"),
 
     # Sidebar with a slider input for number of bins
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
+            sliderInput("dist_data",
                         "Number of bins:",
                         min = 1,
-                        max = 50,
-                        value = 30)
+                        max = 1000,
+                        value = 100), 
+            numericInput("dist_mean",
+                        "Enter mean value:",
+                        min = 1,
+                        max = 100,
+                        value = 10,
+                        step = 1),
+            numericInput("dist_std",
+                        "Enter standard deviation:",
+                        min = 1,
+                        max = 100,
+                        value = 10,
+                        step = 1)
         ),
 
         # Show a plot of the generated distribution
